d5d55f613515e3a4cd343b45df21cab1e79e4474
INFOR\matteou <matteourso100@gmail.com>
2024-06-11 13:00:19 +0200

    Layout sidebar & Mobile Navigation

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
+
+export default function MobileNav() {
+  const pathname = usePathname();
+
+  return (
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/logo-text.svg"
+          alt="Imaginify Logo"
+          height={28}
+          width={180}
+        />
+      </Link>
+
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="Menu"
+                height={32}
+                width={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image
+                  src="/assets/images/logo-text.svg"
+                  alt="Imaginify Logo"
+                  height={23}
+                  width={152}
+                />
+
+                <ul className="header-nav_elements">
+                  {navLinks.map((link) => {
+                    const isActive = pathname === link.route; // TODO: Spostarli in un componente separato e tenere questo un server component
+                    return (
+                      <li
+                        key={link.route}
+                        className={`${
+                          isActive && "gradient-text"
+                        } p-18 flex whitespace-nowrap text-dark-700`}
+                      >
+                        <Link
+                          href={link.route}
+                          className="sidebar-link cursor-pointer"
+                        >
+                          <Image
+                            src={link.icon}
+                            alt={link.label}
+                            height={24}
+                            width={24}
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+      </nav>
+    </header>
+  );
+}
---
+++ components/shared/Sidebar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { navLinks } from "@/constants";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Button } from "../ui/button";
+
+function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="Imaginify Logo"
+            height={28}
+            width={180}
+          />
+        </Link>
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = pathname === link.route; // TODO: Spostarli in un componente separato e tenere questo un server component
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link href={link.route} className="sidebar-link">
+                      <Image
+                        src={link.icon}
+                        alt={link.label}
+                        height={24}
+                        width={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+
+              <li className="flex-center cursor-pointer gap-2 p-4">
+                <UserButton afterSignOutUrl="/" showName />
+              </li>
+            </ul>
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </nav>
+      </div>
+    </aside>
+  );
+}
+
+export default Sidebar;

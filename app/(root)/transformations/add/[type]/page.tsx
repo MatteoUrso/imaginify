70c766a1e63d93315f4bf4d44911d4f5417dadc1
INFOR\matteou <matteourso100@gmail.com>
2024-06-11 16:14:54 +0200

    Updated Transformation Type Page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,21 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
+import { notFound } from "next/navigation";
+import TransformationForm from "@/components/shared/TransformationForm";
 
-function AddTransformationTypePage() {
-  return <div>AddTransformationTypePage</div>;
+function AddTransformationTypePage({ params: { type } }: SearchParamProps) {
+  const transformation = transformationTypes[type];
+
+  // If not found, redirect to 404 page
+  if (!transformation) notFound();
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm />
+    </>
+  );
 }
 
 export default AddTransformationTypePage;

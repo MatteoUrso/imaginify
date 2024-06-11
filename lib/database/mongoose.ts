f38af93093c69f786da0a258a4fc41b17b1d6081
INFOR\matteou <matteourso100@gmail.com>
2024-06-11 13:51:46 +0200

    Init MongoDB

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,6 +7,8 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
+// Used to cache the connection because Next.js is serverless and we want to reuse the connection. If the connection is already cached, we return it.
+// If i don't cache the connection, it will create a new connection for every request.
 let cached: MongooseConnection = (global as any).mongoose;
 if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };

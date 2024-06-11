1235f48377f23b1f2c432ff6a3276b9448ad306a
INFOR\matteou <matteourso100@gmail.com>
2024-06-11 11:52:15 +0200

    Next.js Routing & folder structure

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TransformationPage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  return <div>TransformationPage</div>;
+}
+
+export default TransformationPage;
